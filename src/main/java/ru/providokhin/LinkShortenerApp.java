67e7638ea3cd36fa36b11bdffcb3512e8b681b23
NickProvidokhin <33122330+NickProvidokhin@users.noreply.github.com>
2024-10-29 19:43:10 +0300

    Hw 4 completed (#3)

--- src/main/java/ru/providokhin/LinkShortenerApp.java
+++ src/main/java/ru/providokhin/LinkShortenerApp.java
@@ -2,10 +2,12 @@ package ru.providokhin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.providokhin.loggingstarter.LoggingStarterAutoConfiguration;
 
 @SpringBootApplication
 public class LinkShortenerApp {
     public static void main(String[] args) {
+        LoggingStarterAutoConfiguration.println("It's working!!! Wow!");
         SpringApplication.run(LinkShortenerApp.class, args);
     }
 }
